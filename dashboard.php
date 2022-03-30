df4d29f911ac5f1bd46b9276fc5a7e060b2f748f
Noah <sewellnoah@gmail.com>
2022-03-30 13:06:22 -0400

    need raspberry pi to get into database and libraries

--- dashboard.php
+++ dashboard.php
@@ -1,4 +1,18 @@
-<!DOCTYPE html>
+
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
 <html>
     <head>
       <script type="text/javascript">
